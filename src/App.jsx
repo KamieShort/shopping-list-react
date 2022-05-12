8ad35db8fc032a5ddc209870c939a6b07a258457
Kamie Short <kamie.short@gmail.com>
2022-05-12 08:03:53 -0700

    set up header component, and app.jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import ShoppingList from './views/ShoppingList';
+import Header from './components/Header';
+
 export default function App() {
-  return <h1>Hello World </h1>;
+  return (
+    <div>
+      <Header />
+      <ShoppingList />
+    </div>
+  );
 }
---
+++ src/views/ShoppingList.jsx
@@ -0,0 +1,11 @@
+// import { useReducer } from 'react';
+
+import React from 'react';
+
+export default function ShoppingList() {
+  return (
+    <div>
+      <h1>Shopping List</h1>
+    </div>
+  );
+}
